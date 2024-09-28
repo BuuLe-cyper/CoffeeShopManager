0f2ee7c7a066a3d473cbf4088ade71f79361a9a3
Ho Phuoc Vinh Hung <hophuocvinhhung@gmail.com>
2024-09-28 23:43:27 +0700

    Implement CRUD for Size-Tested

---
+++ CoffeeShop/Pages/SizeTest/Edit.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.DataContext;
+using DataAccess.Models;
+using AutoMapper;
+using BussinessObjects.Services;
+
+namespace CoffeeShop.Pages.SizeTest
+{
+    public class EditModel : PageModel
+    {
+        private readonly ISizeService _sizeService;
+
+
+        public EditModel(ISizeService sizeService)
+        {
+            _sizeService = sizeService;
+        }
+
+        [BindProperty]
+        public Size Size { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size = await _sizeService.GetSize((int)id);
+            if (size == null)
+            {
+                return NotFound();
+            }
+            Size = size;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            try
+            {
+                await _sizeService.UpdateSize(Size);    
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                    throw;
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ CoffeeShop/Pages/SizeTest/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.DataContext;
+using DataAccess.Models;
+using BussinessObjects.Services;
+using AutoMapper;
+using CoffeeShop.ViewModels;
+
+namespace CoffeeShop.Pages.SizeTest
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ISizeService _sizeService;
+        private readonly IMapper _mapper;
+        public IndexModel(ISizeService sizeService , IMapper mapper)
+        {
+            _sizeService = sizeService;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<SizeVM> Size { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            var result =  await _sizeService.GetAllSize();
+            Size = _mapper.Map<IEnumerable<SizeVM>>(result);
+        }
+    }
+}
