80442eb7dd7aca5bc12822d046173fca300f3594
Ho Phuoc Vinh Hung <hophuocvinhhung@gmail.com>
2024-09-28 23:43:27 +0700

    Implement CRUD for Size-Tested

---
+++ BussinessObjects/Services/SizeService.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using BussinessObjects.DTOs;
+using DataAccess.Models;
+using DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessObjects.Services
+{
+    public class SizeService : ISizeService
+    {
+        private readonly ISizeRepository _sizeRepository;
+        private readonly IMapper _mapper;
+        public SizeService(ISizeRepository sizeRepository, IMapper mapper)
+        {
+            _sizeRepository = sizeRepository;
+            _mapper = mapper;
+        }
+        public async Task<bool> AddSize(SizeDto sizeDto)
+        {
+            ArgumentNullException.ThrowIfNull(nameof(sizeDto.sizeName));
+            var entity = _mapper.Map<Size>(sizeDto);
+            return await _sizeRepository.AddSizeEntity(entity);
+        }
+
+        public async Task<IEnumerable<Size>> GetAllSize()
+        {
+            var sizes = await _sizeRepository.GetAllAsync(s => s.IsDeleted == false && s.IsActive == true);
+            return sizes;
+        }
+
+        public async Task<Size> GetSize(int sizeID)
+        {
+            var size = await _sizeRepository.GetAsync(s => s.IsDeleted == false && s.IsActive == true && s.SizeID == sizeID);
+            return size;
+        }
+
+        public async Task<Size> GetSizeName(string sizeName)
+        {
+            var size = await _sizeRepository.GetAsync(s => s.IsDeleted == false && s.IsActive == true && sizeName.Contains(sizeName));
+            return size;
+        }
+
+        public async Task<bool> SoftDeleteSize(int sizeID)
+        {
+            return await _sizeRepository.SoftDeleteSizeEntity(sizeID);
+        }
+
+        public async Task<bool> UpdateSize(Size size)
+        {
+            ArgumentNullException.ThrowIfNull(nameof(size));
+            var sizeExists = await _sizeRepository.GetAsync(s => s.SizeID == size.SizeID && s.IsDeleted == false && s.IsActive == true) ?? throw new Exception("Size Not Found");
+            if (sizeExists != null)
+            {
+                // ensuring no permission modify this field
+                size.IsDeleted = false;
+                size.IsActive = true;
+                size.CreateDate = sizeExists.CreateDate;
+                size.ModifyDate = sizeExists.ModifyDate;
+
+                await _sizeRepository.UpdateAsync(size);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CoffeeShop/Pages/SizeTest/Delete.cshtml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.DataContext;
+using DataAccess.Models;
+using AutoMapper;
+using BussinessObjects.Services;
+
+namespace CoffeeShop.Pages.SizeTest
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ISizeService _sizeService;
+
+        public DeleteModel(ISizeService sizeService)
+        {
+            _sizeService = sizeService;
+        }
+        [BindProperty]
+        public Size Size { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size = await _sizeService.GetSize((int)id);
+
+            if (size == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Size = size;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var size = await _sizeService.GetSize((int)id);
+            if (size != null)
+            {
+                Size = size;
+                var isRemove =  await _sizeService.SoftDeleteSize(size.SizeID);
+                if (!isRemove)
+                {
+                    ModelState.AddModelError(string.Empty, "Unable to delete size. Please try again.");
+                    return Page();
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
