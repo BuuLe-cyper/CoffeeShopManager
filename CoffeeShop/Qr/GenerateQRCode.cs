71139c884f899bf0d88eeb8557e7332d0ffcdcb5
BuuLe-cyper <142316601+BuuLe-cyper@users.noreply.github.com>
2024-10-03 00:06:35 +0700

    debug qr code

--- CoffeeShop/Qr/GenerateQRCode.cs
+++ CoffeeShop/Qr/GenerateQRCode.cs
@@ -14,30 +14,30 @@ namespace CoffeeShop.Qr
 
         public string GenerateQRCodeForTable(int tableId)
         {
-            // Get the current request's scheme and host to build the URL dynamically
+            return $"data:image/png;base64,";
+            //// Get the current request's scheme and host to build the URL dynamically
             //var request = _httpContextAccessor.HttpContext.Request;
             //string baseUrl = $"{request.Scheme}://{request.Host}";
             //string chatUrl = $"{baseUrl}/Customer/Shopping/Index/{tableId}";
 
-            string chatUrl = $"/Customer/Shopping/Index/{tableId}";
-            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
-            {
-                QRCodeData qrCodeData = qrGenerator.CreateQrCode(chatUrl, QRCodeGenerator.ECCLevel.Q);
-                using (QRCode qrCode = new QRCode(qrCodeData))
-                {
-                    // Use the GetGraphic method from QRCoder
-                    using (Bitmap qrCodeImage = qrCode.GetGraphic(20))
-                    {
-                        using (MemoryStream ms = new MemoryStream())
-                        {
-                            qrCodeImage.Save(ms, ImageFormat.Png);
-                            byte[] imageBytes = ms.ToArray();
-                            string base64String = Convert.ToBase64String(imageBytes);
-                            return $"data:image/png;base64,{base64String}";
-                        }
-                    }
-                }
-            }
+            //using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            //{
+            //    QRCodeData qrCodeData = qrGenerator.CreateQrCode(chatUrl, QRCodeGenerator.ECCLevel.Q);
+            //    using (QRCode qrCode = new QRCode(qrCodeData))
+            //    {
+            //        // Use the GetGraphic method from QRCoder
+            //        using (Bitmap qrCodeImage = qrCode.GetGraphic(20))
+            //        {
+            //            using (MemoryStream ms = new MemoryStream())
+            //            {
+            //                qrCodeImage.Save(ms, ImageFormat.Png);
+            //                byte[] imageBytes = ms.ToArray();
+            //                string base64String = Convert.ToBase64String(imageBytes);
+            //                return $"data:image/png;base64,{base64String}";
+            //            }
+            //        }
+            //    }
+            //}
         }
     }
 }
