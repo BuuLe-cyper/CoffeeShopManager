87218e6d33c164cab0236ed917555600b82398ed
Ho Phuoc Vinh Hung <hophuocvinhhung@gmail.com>
2024-11-04 16:29:12 +0700

    Fix UI Menu

--- CoffeeShop/Areas/Shared/Pages/Menu/ListMenu.cshtml.cs
+++ CoffeeShop/Areas/Shared/Pages/Menu/ListMenu.cshtml.cs
@@ -14,23 +14,28 @@ namespace CoffeeShop.Areas.Shared.Pages.Menu
     public class ListMenuModel : PageModel
     {
         private readonly IProductSizesService _productSizesService;
+        private readonly ICategoryService _categoryService;
         private readonly IMapper _mapper;
-        public ListMenuModel(IProductSizesService productSizesService, IMapper mapper)
+        public ListMenuModel(IProductSizesService productSizesService, IMapper mapper, ICategoryService categoryService)
         {
             _productSizesService = productSizesService;
             _mapper = mapper;
+            _categoryService = categoryService;
         }
         public PaginatedList<ProductSizeVM> ProductSize { get; set; }
         public string SearchQuery { get; set; }
         public string CurrentSort { get; set; }
         public int PageIndex { get; set; } = 1;
-
-        public async Task OnGetAsync(string searchQuery, int pageIndex = 1, int pageSize = 3)
+        public IEnumerable<CategoryVM> Category { get; set; } = default!;
+        public async Task OnGetAsync(string searchQuery, int pageIndex = 1, int pageSize = 3, int cateId = 0)
         {
             SearchQuery = searchQuery;
             var result = await _productSizesService.GetAllProductSizes();
+            var resultCate = cateId == 0
+                              ? result
+                              : result.Where(it => it.Product.CategoryID == cateId);
 
-            var filteredResults = result
+            var filteredResults = resultCate
                 .Where(x => !x.Size.IsDeleted &&
                             (string.IsNullOrEmpty(searchQuery) ||
                              x.Product.ProductName.Contains(searchQuery, StringComparison.OrdinalIgnoreCase) ||
@@ -51,6 +56,8 @@ namespace CoffeeShop.Areas.Shared.Pages.Menu
             var items = pageGroups.SelectMany(g => g.Select(x => _mapper.Map<ProductSizeVM>(x))).ToList();
 
             ProductSize = new PaginatedList<ProductSizeVM>(items, count, pageIndex, pageSize);
+            var results = await _categoryService.GetAllCategory();
+            Category = _mapper.Map<IEnumerable<CategoryVM>>(results);
         }
     }
 }
--- CoffeeShop/wwwroot/js/site.js
+++ CoffeeShop/wwwroot/js/site.js
@@ -2,3 +2,45 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    function openTab(evt, id) {
+        $('.tm-tab-content').hide();
+    $('#' + id).show();
+    $('.tm-tab-link').removeClass('active');
+    $(evt.currentTarget).addClass('active');
+  }
+    function highlightMenu(menuItem) {
+        $('.tm-page-link').removeClass('active');
+    menuItem.addClass('active');
+  }
+
+    function showPage(page) {
+        $('.tm-page-content').hide();
+    page.show();
+  }
+
+$(document).ready(function () {
+
+    /***************** Pages *****************/
+
+    initPage();
+
+    $('.tm-page-link').click(function (event) {
+
+        if (window.innerWidth > 991) {
+            event.preventDefault();
+        }
+
+        highlightMenu($(event.currentTarget));
+        showPage($(event.currentTarget.hash));
+    });
+
+
+    /***************** Tabs *******************/
+
+    $('.tm-tab-link').on('click', e => {
+        e.preventDefault();
+        openTab(e, $(e.target).data('id'));
+    });
+
+    $('.tm-tab-link.active').click(); // Open default tab
