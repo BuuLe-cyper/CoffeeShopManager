3457f37b899ed4c64ec37d7d29bc88fb9961eec0
BuuLe-cyper <buule.210303@gmail.com>
2024-11-12 20:22:52 +0700

    Change url payment

--- CoffeeShop/ViewModels/CreatePaymentLinkRequest.cs
+++ CoffeeShop/ViewModels/CreatePaymentLinkRequest.cs
@@ -6,7 +6,7 @@
         //show about "userName" payment
         public string Description { get; set; } = "BY PAYOS";
         public decimal TotalPrice { get; set; }
-        public string returnUrl { get; set; } = "https://localhost:7197/Shared/DemoPaymentReturn/Success";
-        public string cancelUrl { get; set; } = "https://localhost:7197/Shared/DemoPaymentReturn/Fail";
+        public string returnUrl { get; set; } = "https://coffeeshopprn221.azurewebsites.net/Shared/DemoPaymentReturn/Success";
+        public string cancelUrl { get; set; } = "https://coffeeshopprn221.azurewebsites.net/Shared/DemoPaymentReturn/Fail";
     }
 }
