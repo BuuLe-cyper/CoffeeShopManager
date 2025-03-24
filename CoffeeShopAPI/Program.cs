fa6e3afcb0e270c8b216abb44977b7ef0bb4123b
Ho Phuoc Vinh Hung <hophuocvinhhung@gmail.com>
2025-03-24 21:06:46 +0700

    add api

--- CoffeeShopAPI/Program.cs
+++ CoffeeShopAPI/Program.cs
@@ -1,6 +1,12 @@
 
+using BusinessObjects.Services;
+using BussinessObjects.AutoMapper;
+using BussinessObjects.ImageService;
 using BussinessObjects.Services;
+using BussinessObjects.Utility;
+using CoffeeShop.AutoMapper;
 using DataAccess.DataContext;
+using DataAccess.Qr;
 using DataAccess.Repositories;
 using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
@@ -30,14 +36,46 @@ namespace CoffeeShopAPI
                            .AllowAnyHeader();
                 });
             });
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+            builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("SmtpSettings"));
+
+            builder.Services.Configure<FireBaseOptions>(builder.Configuration.GetSection("FireBase"));
+
+            builder.Services.AddTransient<MailService>();
+
+            builder.Services.AddTransient(typeof(IImageService), typeof(ImageService));
+
+            builder.Services.AddHttpContextAccessor();
 
             //Add Services
             builder.Services.AddScoped<ITableService, TableService>();
             builder.Services.AddScoped<IMessService, MessService>();
-
+            builder.Services.AddScoped(typeof(ISizeService), typeof(SizeService));
+            builder.Services.AddScoped(typeof(ICategoryService), typeof(CategoryService));
+            builder.Services.AddScoped(typeof(IProductService), typeof(ProductService));
+            builder.Services.AddScoped(typeof(IProductSizesService), typeof(ProductSizesService));
+            builder.Services.AddScoped(typeof(IOrderService), typeof(OrderService));
+            builder.Services.AddScoped(typeof(IOrderDetailService), typeof(OrderDetailService));
             //Add Repositories
             builder.Services.AddScoped<ITableRepository, TableRepository>();
             builder.Services.AddScoped<IMessRepository, MessRepository>();
+            builder.Services.AddScoped(typeof(ISizeRepository), typeof(SizeRepository));
+            builder.Services.AddScoped(typeof(ICategoryRepository), typeof(CategoryRepository));
+            builder.Services.AddScoped(typeof(IProductRepository), typeof(ProductRepository));
+            builder.Services.AddScoped(typeof(IProductSizesRepository), typeof(ProductSizesRepository));
+            builder.Services.AddScoped(typeof(IOrderRepository), typeof(OrderRepository));
+            builder.Services.AddScoped(typeof(IOrderDetailRepository), typeof(OrderDetailRepository));
+    
+            // AutoMapper
+            builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+            builder.Services.AddAutoMapper(typeof(MappingProfileView).Assembly);
+
+
+            // Add QR Code
+            builder.Services.AddScoped<GenerateQRCode>();
+
 
             builder.Services.AddControllers()
             .AddOData(options => options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(100))
