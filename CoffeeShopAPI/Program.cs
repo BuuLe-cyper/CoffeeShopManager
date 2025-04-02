92e8bda613d898c41aa0f6b636844100f15bc0f2
BuuLe-cyper <buule.210303@gmail.com>
2025-04-02 22:54:43 +0700

    Update base url production

--- CoffeeShopAPI/Program.cs
+++ CoffeeShopAPI/Program.cs
@@ -101,11 +101,9 @@ namespace CoffeeShopAPI
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
